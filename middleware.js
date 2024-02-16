e014d25684db727679341bc7bb8aac1d4431cfe8
Jayk5 <jayantkumar20@cse.iiitp.ac.in>
2024-02-16 20:47:23 +0530

    feat: add middeware

---
+++ middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server"
+
+export async function middleware(request) {
+  const { pathname } = request.nextUrl
+  const paths = ['/', '/product']
+  if (pathname !== '/' && !pathname.startsWith('/product')) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+}
